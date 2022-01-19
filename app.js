beef15245c413a35e3071f99c7d861eb70473168
timoor1029 <55950556+timoor1029@users.noreply.github.com>
2022-01-19 20:09:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Auto Scaling Demo App</h1> <h4>Message: Success</h4> <p>Version: 1.0.0</p>');
+  res.send('<html>
+    <head>
+        <title>Fayhaa Khair</title>
+    </head>
+    <body>
+        <p>I Love You</p>
+    </body>
+</html>');
 })
 
 app.listen(port, () => {
